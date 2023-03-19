04c280b2d3d47e1adc9b46cc11bdc78cb8bed401
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-03-19 14:16:50 -0600

    Just started game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 //import logo from './logo.svg';
 //import { Counter } from './features/counter/Counter';
 import './App.css';
@@ -10,15 +12,10 @@ import Footer from './components/Footer';
 import {createElement,useState,useEffect,useRef} from 'react';
 import {CreatedElement} from './components/Misc';
 import {CreatedElement2} from './components/Misc';
-import {New_Button} from './components/Misc';
-import {New_Button2} from './components/Misc';
-import  My_Form  from './components/My_Form';
-import CampsiteDirectoryPage from './pages/CampsitesDirectoryPage';
-import {Routes, Route} from  'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
+import style from '../src/app/shared/StudentList.module.css';
+import lui from '../src/app/assets/img/a1.gif';
+import luistand from '../src/app/assets/img/stand.gif';
+import arena from '../src/app/assets/img/arena.png';
 
 
 const test2 = (name) =>{
@@ -58,36 +55,20 @@ function App() {
     setTimeout(peaceOnEarth,2000);
   },[name]);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+}, [dispatch]);
+
   return (
-    <>
-    <div ref={divEl} onClick={() => test1(true)} className="App">
-            <Button onclick={() => setName('Marcus')} style={{color:'blue', backgroundColor:'red'}} >This is my New button</Button>
-            <Button onClick={() => x+=2} style={basicBtn} >This is my New button</Button>
+   
+
+    <div className={style.arena}>
             
-            <Button className='primary' color="info" >This is my New button</Button>
-            <CreatedElement person={{ name: {name}, imageId: '1bX5QH6'}} test={test2({name})} testx={name}  />
-            <CreatedElement2 props={name}  />
-            <CreatedElement2 props={name}  />
-            <New_Button buttonName='Jeremys Button'/>
-            </div> 
-            <Button className='primary' color="info" onClick={() => setName('Jade') }>This is my New button</Button>
-            
-            <New_Button2 />
-            <Header />
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='contact' element={<ContactPage />} />
-              <Route path='directory' element={<CampsiteDirectoryPage />} />
-              <Route path='about' element={<AboutPage />} />
-              <Route 
-                path='directory/:campsiteId'
-                element={<CampsiteDetailPage />}
-              />
-            </Routes>
-            <My_Form />
-            <Footer />
-        
-        </>
+            <img className={style.fighter} src={luistand}></img>
+         
+    </div> 
     
   )
 }
